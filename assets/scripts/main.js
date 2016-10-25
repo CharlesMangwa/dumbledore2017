01bb6a383bfa3b65700a2304e5d8cf4bc4230af2
Charles Mangwa <charlesmangwa@gmail.com>
2016-10-25 11:04:27 +0200

    feat(assets | sass): add custom utils

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,11 +3,3 @@
  */
 const array = [0, 1, 2]
 console.log(`ES8 support: ${array.includes(2)}`)
-
-(function($) {
-
-    // Picture element HTML5 shiv
-    document.createElement('picture')
-
-
-})(jQuery)
