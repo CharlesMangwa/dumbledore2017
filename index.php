4aa75e6441c310e5be27236f8b190e45aa4139df
toinelin <antoine.lin@hetic.net>
2016-11-12 19:02:30 +0100

    update(integration|landing): integration before checkout

--- index.php
+++ index.php
@@ -12,24 +12,29 @@
 
 </div>
 
-<main class="Homepage__content">
+<main class="Homepage__content MainWrapper">
+  <ul class="Homepage__content__section">
+    <li class="Homepage__content__section__about">
+      <?php if ( have_posts() ) : ?>
 
-    <?php if ( have_posts() ) : ?>
+          <?php while ( have_posts() ) : the_post(); ?>
+              <?php  get_template_part( 'templates/partials/content-landing'); ?>
+          <?php endwhile; ?>
 
-        <?php while ( have_posts() ) : the_post(); ?>
-            <?php  get_template_part( 'templates/partials/content-landing'); ?>
-        <?php endwhile; ?>
+          <?php if ($wp_query->max_num_pages > 1) : ?>
+              <?php get_template_part( 'templates/partials/pager' ); ?>
+          <?php endif; ?>
 
-        <?php if ($wp_query->max_num_pages > 1) : ?>
-            <?php get_template_part( 'templates/partials/pager' ); ?>
-        <?php endif; ?>
+      <?php else : ?>
 
-    <?php else : ?>
+          <?php get_template_part( 'templates/partials/content', 'none' ); ?>
 
-        <?php get_template_part( 'templates/partials/content', 'none' ); ?>
+      <?php endif; ?>
+    </li>
+    <li class="Homepage__content__section__dunno">
 
-    <?php endif; ?>
-
+    </li>
+  </ul>
 </main>
 
 <?php get_footer(); ?>
