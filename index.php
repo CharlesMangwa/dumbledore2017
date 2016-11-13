8ddad433587c8382742780445d644e529ef3851e
CMarzin <corentinmarzin@gmail.com>
2016-11-13 01:50:35 +0100

    feat(integration|pageactu) template style final

--- index.php
+++ index.php
@@ -6,29 +6,16 @@
 
 <?php get_header(); ?>
 
-<main class="Homepage__content MainWrapper">
-  <ul class="Homepage__content__section">
-    <li class="Homepage__content__section__about">
       <?php if ( have_posts() ) : ?>
 
           <?php while ( have_posts() ) : the_post(); ?>
-              <?php  get_template_part( 'templates/partials/content-landing'); ?>
+              <?php  get_template_part( 'templates/partials/content-actu'); ?>
           <?php endwhile; ?>
-
-          <?php if ($wp_query->max_num_pages > 1) : ?>
-              <?php get_template_part( 'templates/partials/pager' ); ?>
-          <?php endif; ?>
 
       <?php else : ?>
 
           <?php get_template_part( 'templates/partials/content', 'none' ); ?>
 
       <?php endif; ?>
-    </li>
-    <li class="Homepage__content__section__dunno">
-
-    </li>
-  </ul>
-</main>
 
 <?php get_footer(); ?>
