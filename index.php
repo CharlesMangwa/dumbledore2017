4ab2f2eb22238348e005beb1613c9516b5bd7cb4
CMarzin <corentinmarzin@gmail.com>
2016-11-14 23:03:13 +0100

    feat(dynamique|pageactu) dynamis content in back office

--- index.php
+++ index.php
@@ -5,17 +5,40 @@
 ?>
 
 <?php get_header(); ?>
+<!-- <div class="Homepage">
+  <section class="Homepage__heading">
 
+  </section>
+
+</div>
+
+Ca reste tout le temps, pour toutes les pages qu'on inté.
+Ca n'est jamais remplacé.
+-->
+
+<main class="Homepage__content MainWrapper">
+  <ul class="Homepage__content__section">
+    <li class="Homepage__content__section__about">
       <?php if ( have_posts() ) : ?>
 
           <?php while ( have_posts() ) : the_post(); ?>
-              <?php  get_template_part( 'templates/partials/content-actu'); ?>
+              <?php  get_template_part( 'templates/partials/content-landing'); ?>
           <?php endwhile; ?>
+
+          <?php if ($wp_query->max_num_pages > 1) : ?>
+              <?php get_template_part( 'templates/partials/pager' ); ?>
+          <?php endif; ?>
 
       <?php else : ?>
 
           <?php get_template_part( 'templates/partials/content', 'none' ); ?>
 
       <?php endif; ?>
+    </li>
+    <li class="Homepage__content__section__dunno">
+
+    </li>
+  </ul>
+</main>
 
 <?php get_footer(); ?>
--- templates/partials/content-actu.php
+++ templates/partials/content-actu.php
@@ -1,4 +1,6 @@
-<div class="page-actu">
+<article id="post-<?php the_ID(); ?>" class="post content content-post">
+
+  <div class="page-actu">
   <h3 class="page-actu-ornement__top-left">d</h3>
   <section class="page-actu__heading">
     <h3 class="page-actu__subtitle">RÉSEAUX & ACTU</h3>
@@ -8,15 +10,16 @@
 
     <div class="page-actu__firstarticle">
       <h3 class="page-actu-firstarticle-img__ornement">d</h3>
-      <img class="page-actu-firstarticle__img" src="https://unsplash.it/600/400" alt="" />
+      <div class="page-actu-firstarticle__img"><?php the_post_thumbnail(); ?></div>
+      <!-- <img class="" src="" alt="" /> -->
       <div class="page-actu-firstarticle__background"></div>
       <div class="page-actu-firstarticle__content">
-        <h4 class="page-actu-article__date page-actu-article__date-red">GENMAG . MAI 2016</h4>
-        <h3 class="page-actu-article__title">TITRE</h3>
-        <h3 class="page-actu-article__title">ACTU N°1</h3>
+        <h4 class="page-actu-article__date page-actu-article__date-red"><?php the_date(); ?></h4>
+        <h3 class="page-actu-article__title"><?php the_title(); ?></h3>
+        <h3 class="page-actu-article__title"><?php the_field('sous-titre') ?></h3>
         <h3 class="page-actu-article__ornement">d</h3>
-        <p class="page-actu-article__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <h4 class="page-actu-article__plus">Lire la suite</h4>
+        <p class="page-actu-article__text"><?php the_content(); ?></p>
+        <h4 class="page-actu-article__plus"><a href="#">Lire la suite</a></h4>
       </div>
     </div>
 
@@ -90,3 +93,4 @@
   </section>
   <h3 class="page-actu-ornement__bottom-right">d</h3>
 </div>
+</article>
