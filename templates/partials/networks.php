2f0f3dfbfebf1c8c06f0006b64612aa448b48cee
toinelin <antoine.lin@hetic.net>
2016-11-15 23:06:34 +0100

    refactor(shareicons | templates): set templates for network sharing icons

---
+++ templates/partials/networks.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Displays posts content
+ */
+?>
+<ul class="NetworkList">
+  <a href="#"><li class="button">partager<span><img src="<?php bloginfo( 'template_url' )?>/assets/images/facebook-icon.svg" alt="partager sur facebook" /></span></li></a>
+  <a href="#"><li class="button">tweeter<span><img src="<?php bloginfo( 'template_url' )?>/assets/images/twitter-icon.svg" alt="partager sur twitter" /></span></li></a>
+  <a href="#"><li class="button">email<span><img src="<?php bloginfo( 'template_url' )?>/assets/images/mail-icon.svg" alt="partager sur facebook" /></span></li></a>
+</ul>
